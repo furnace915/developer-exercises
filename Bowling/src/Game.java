a6f4f3cd654e859036d12b3dd4dfdc4f913f0030
Velandra <bvelandr@mifhbvelandr-m1.corp.local>
2014-04-21 15:37:51 -0400

    missed a file in previous commit

--- Bowling/src/Game.java
+++ Bowling/src/Game.java
@@ -3,33 +3,13 @@ import java.util.List;
 public class Game {
 
 	public int bowl(List<Frame> frames) {
-		
+		ScoreHandler score = new ScoreHandler();
 		int total = 0;
+		
 		preCalculateScoresForAllFrames(frames);
 
 		for (int i = 0; i < frames.size(); i++) {
-			if (i < 10) {
-				if (frames.get(i).hasSpare()) {
-					total += 10;
-					total += frames.get(i + 1).getRoll1();
-				} else if (!frames.get(i).hasSpare() && !frames.get(i).hasStrike()) {
-					total += frames.get(i).roll();
-				}
-				
-				if (frames.get(i).hasStrike()) {
-					total += 10;
-					if (!frames.get(i + 1).hasSpare() && !frames.get(i + 1).hasStrike()){
-						total += frames.get(i+1).roll();
-					}
-					else if (!frames.get(i + 1).hasSpare() && frames.get(i + 1).hasStrike()){
-						total += 10;
-						total += frames.get(i + 2).getRoll1();
-					}
-					else if (frames.get(i + 1).hasSpare() && !frames.get(i + 1).hasStrike()){
-						total += 10;
-					}
-				}
-			}			
+			total = score.handleScoring(frames, i);			
 		}		
 		return total;
 	}
